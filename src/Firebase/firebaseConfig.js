30e6fbcafa2954baa4ba9891c9f8eeb220abfece
Gatesvert81 <63714591+Gatesvert81@users.noreply.github.com>
2022-09-09 10:57:14 +0000

    update

--- src/Firebase/firebaseConfig.js
+++ src/Firebase/firebaseConfig.js
@@ -21,12 +21,12 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD0XjolsxDFkRWfwUNcBL0vIqfC5dI-EVY",
-  appId: process.env.FIREBASE_APP_ID,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  authDomain: "local-pharma-70836.firebaseapp.com",
+  projectId: "local-pharma-70836",
+  storageBucket: "local-pharma-70836.appspot.com",
+  messagingSenderId: "913127224920",
+  appId: "1:913127224920:web:fcb2fd0e4260b4ef8efc2d",
+  measurementId: "G-X7FN880FDJ"
 };
 // dfa97485dddd82f1d3d38bfb00673c7e8cda162d
 
